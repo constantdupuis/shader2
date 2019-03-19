5e1bc22aee782461c83970fd8056a02a104ea5a4
Constant Dupuis <constant.dupuis@gmail.com>
2019-03-19 13:11:10 +0100

    Use colour palette to tint final pixels
    ADD: palettes source and PNG

--- 2DMetaballs/2Dmetaballs.glsl
+++ 2DMetaballs/2Dmetaballs.glsl
@@ -23,8 +23,8 @@ void main() {
     vec2 tuv = gl_FragCoord.xy/u_resolution.xy;
     vec2 uv = (2.*gl_FragCoord.xy-u_resolution.xy)/u_resolution.y;
 
-    //vec4 colorT = texture2D(u_texture_0, uv);
-    //gl_FragColor = vec4(vec3(colorT.b), 1.0);
+    // vec4 colorT = texture2D(u_texture_0, uv);
+    // gl_FragColor = vec4(vec3(colorT.r,colorT.g, colorT.b ), 1.0);
 
     float m = 0.0;
     float t = u_time/2.0;
@@ -34,19 +34,23 @@ void main() {
         vec2 n = N22(vec2(i));
         vec2 p  = sin(n*t);
 
-        n = (n+vec2(1.0))/8.0;
+        //n = (n+vec2(1.0))/2.0;
 
         float d = length(uv-p);
-        m += smoothstep(n.x, 0.001, d);
+        //m += smoothstep(n.x, 0.001, d);
+        m += smoothstep(0.3, 0.001, d);
     }
+    //m = clamp(m, 0.0, 1.0);
 
-    m = smoothstep(0.4,0.41, m);
+    //m = smoothstep(0.4,0.41, m);
     
-    //m = sin(m*15.);
+    m = sin(m*2.0);
+    m = (m+1.0)*0.5;
     
-    vec3 c = hue2rgb( m );
-    //vec3 col = c * m;
+    //vec4 finalColor = vec4(vec3(m), 1.0);
 
-    vec3 col = vec3(m);
-    gl_FragColor = vec4(col, 1.0);
+    vec4 colorT = texture2D(u_texture_0, vec2(m, 10.));
+    vec4 finalColor = vec4( colorT.r, colorT.g, colorT.b, 1.0 );
+
+    gl_FragColor = finalColor;
 }
