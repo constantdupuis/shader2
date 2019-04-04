23c7e8c5607174df287eb268be407c48aebfded3
Constant Dupuis <constant.dupuis@gmail.com>
2019-04-04 21:48:52 +0200

    Add projects
    Add 2 Plasmama projects and a Kind-Of-Ink

--- KindOfInkBW/kindOfInkBW.glsl
+++ KindOfInkBW/kindOfInkBW.glsl
@@ -28,11 +28,9 @@ void main() {
 
     vec4 disp = texture2D(u_texture_1, tuv);
     vec4 dispb =  disp -  0.5;
-    // gl_FragColor = vec4(vec3(colorT.r,colorT.g, colorT.b ), 1.0);
-    //uv += dispb.bg*u_strength;
-    ///uv += dispb.bg*(sin(u_time*(2.))/5.);
+   
+    uv += dispb.bg*sin (u_time)*0.5;
 
-    //uv += disp.xy*(16.*u_strength);
     float m = 0.0;
     float t = 100.+u_time/(20.0);
 
@@ -41,11 +39,9 @@ void main() {
         vec2 n = N22(vec2(i));
         vec2 p  = sin(n*t);
 
-        //n = (n+vec2(1.0))/2.0;
-
+   
         float d = length(uv-p);
         
-        //m += smoothstep(n.x, 0.001, d);
         m += smoothstep(0.6, 0.001, d);
     }
     //m = clamp(m, 0.0, 1.0);
@@ -57,11 +53,11 @@ void main() {
     
     //vec4 finalColor = vec4(vec3(m), 1.0);
 
-    vec4 colorT = texture2D(u_texture_0, vec2(m, 10.));
+    //vec4 colorT = texture2D(u_texture_0, vec2(m, 10.));
     //colorT.rgb *= disp.bbb;
-    vec4 finalColor = vec4( colorT.r, colorT.g, colorT.b, 1.0 );
+    //vec4 finalColor = vec4( colorT.r, colorT.g, colorT.b, 1.0 );
 
-    //vec4 finalColor = vec4( m, m, m, 1.0 );
+    vec4 finalColor = vec4( m, m, m, 1.0 );
 
 
     gl_FragColor = finalColor;
