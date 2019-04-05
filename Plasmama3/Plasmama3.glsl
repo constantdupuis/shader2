67a2be0f43e89582317dc73a631d22f8ab5b6f2b
Constant Dupuis <constant.dupuis@gmail.com>
2019-04-05 16:12:25 +0200

    Add a third layer in plasmama 3

--- Plasmama3/Plasmama3.glsl
+++ Plasmama3/Plasmama3.glsl
@@ -5,6 +5,7 @@ uniform vec2 u_mouse;
 uniform float u_time;
 uniform sampler2D u_texture_0; 
 uniform sampler2D u_texture_1;
+uniform sampler2D u_texture_2;
 uniform float u_strength;
 uniform float u_strength2;
 
@@ -25,12 +26,19 @@ vec3 hue2rgb(float hue) {
 
 void main() {
 
-    vec2 uv = (2.*gl_FragCoord.xy-u_resolution.xy)/u_resolution.y; // pixel uv, range -1 to 1 and compensate screen aspect ratio
+    // pixel uv, range -1 to 1 and compensate screen aspect ratio
+    vec2 uv = (2.*gl_FragCoord.xy-u_resolution.xy)/u_resolution.y; 
+
     vec2 uv2 = uv; 
     uv2.x *= -1.0;
+    
+    vec2 uv3 = uv; 
+    uv3.y *= -1.0;
 
     float m = 0.0; // gradiant accumulator
     float m2 = 0.0; // gradiant accumulator 2
+    float m3 = 0.0; // gradiant accumulator 3
+
     float t = 100.+u_time/(20.0); // time base
 
     // create "particles"
@@ -45,10 +53,12 @@ void main() {
         // give a gradiant range from 0 to > 1
         float d = length(uv-p);
         float d2 = length(uv2-p);
+        float d3 = length(uv3-p);
 
         // limit gradiant from 0 to 1 and add it to  m
         m += smoothstep(0.6, 0.001, d);
         m2 += smoothstep(0.6, 0.001, d2);
+        m3 += smoothstep(0.6, 0.001, d3);
     }
     // m range from 0 to > 1
     m = sin(m*3.0); // get gradiant back in -1, 1 range
@@ -56,12 +66,18 @@ void main() {
     
     m2 = sin(m2*10.0);
     m2 = (m2+1.0)*0.5;
+    
+    m3 = sin(m3*10.0);
+    m3 = (m3+1.0)*0.5;
 
     // use gradiant to pick texture color
-    vec4 colorT = texture2D(u_texture_0, vec2(m, 10.));
+    vec4 colorT =  texture2D(u_texture_0, vec2(m,  10.));
     vec4 colorT2 = texture2D(u_texture_1, vec2(m2, 10.));
-    vec4 finalColor = colorT+(colorT2*(1.0-colorT.a));
-    //vec4 finalColor = colorT+colorT2;
+    vec4 colorT3 = texture2D(u_texture_2, vec2(m3, 10.));
+
+    vec4 finalColor = colorT+(colorT2*(1.0-colorT.a))+(colorT3*(1.0-colorT2.a+1.0-colorT.a));
+    //vec4 finalColor = colorT+(colorT2*(1.0-colorT.a));
+    //vec4 finalColor = vec4(uv.x, 0.0,0.0,1.0);
 
     gl_FragColor = finalColor;
 }
